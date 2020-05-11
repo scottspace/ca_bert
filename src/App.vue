ba30290e0c5572f121f7055f8c5664c4a8db7a0d
Scott Penberthy <scott@scott.ai>
2020-05-10 17:25:59 -0700

    adding phone body

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="app">
-    Let's build an Instagram Clone!
+    <div class="app-phone">
+      <div class="phone-header">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1211695/vue_gram_logo_cp.png" />
+      </div>
+      <phone-body />
+      <div class="phone-footer">
+       <div class="home-cta">
+        <i class="fas fa-home fa-lg"></i>
+       </div>
+       <div class="upload-cta">
+        <i class="far fa-plus-square fa-lg"></i>
+       </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import PhoneBody from "./components/PhoneBody";
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    "phone-body": PhoneBody
+  }
 };
 </script>
 
---
+++ src/components/PhoneBody.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="phone-body">
+    This is the Phone Body
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PhoneBody"
+};
+</script>
+
+<style lang="scss" src="../styles/phone-body.scss">
+// Styles from stylesheet
+</style>
